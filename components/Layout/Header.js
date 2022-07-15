4c207ab219fc0ff164f7caf8f997f4caed86066a
Aleksandra Vukusic <vukusic.aleksandra@gmail.com>
2022-07-15 09:43:55 +0200

    nav

--- components/Layout/Header.js
+++ components/Layout/Header.js
@@ -43,13 +43,19 @@ const Header = () => {
           </Link>
         </div>
       </div>
-      <div className={`${classes.Nav} ${navOpen ? classes.NavOpen : ""}`}>
-        <NavItem navText="ABOUT" imgSrc="/images/about_nav.png" />
-        <NavItem navText="PROCESS" imgSrc="/images/process_nav.png" />
-        <NavItem navText="PROJECTS" imgSrc="/images/project_nav.png" />
-        <NavItem navText="SERVICES" imgSrc="/images/services_nav.png" />
-        <NavItem navText="BLOG" imgSrc="/images/blog_nav.png" />
-        <NavItem navText="CONTACT" imgSrc="/images/contact_nav.png" />
+      <div
+        className={`${classes.Nav} ${
+          navOpen ? classes.NavOpen : classes.NavClose
+        }`}
+      >
+        <div className={classes.NavItemWrapper}>
+          <NavItem navText="ABOUT" imgSrc="/images/about_nav.png" />
+          <NavItem navText="PROCESS" imgSrc="/images/process_nav.png" />
+          <NavItem navText="PROJECTS" imgSrc="/images/project_nav.png" />
+          <NavItem navText="SERVICES" imgSrc="/images/services_nav.png" />
+          <NavItem navText="BLOG" imgSrc="/images/blog_nav.png" />
+          <NavItem navText="CONTACT" imgSrc="/images/contact_nav.png" />
+        </div>
       </div>
     </header>
   );
