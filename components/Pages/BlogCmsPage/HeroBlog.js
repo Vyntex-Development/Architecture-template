9b2fb1ff248ca2ed6f9e56f550143452da827c8c
Aleksandra Vukusic <vukusic.aleksandra@gmail.com>
2022-07-21 10:50:51 +0200

    search

--- components/Pages/BlogCmsPage/HeroBlog.js
+++ components/Pages/BlogCmsPage/HeroBlog.js
@@ -4,6 +4,7 @@ import Image from "next/image";
 const HeroBlog = () => {
   return (
     <div>
+      <div className={classes.HeroNav}>subscribe</div>
       <div className={classes.HeroWrapper}>
         <div className={classes.HeroLeft}>
           <h1>Underfoot and reaching into the light</h1>
